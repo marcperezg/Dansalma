3564152129188365e9d4734480d5da8b537245e2
Marc <marc.perez.guerrero@estudiantat.upc.edu>
2023-07-05 01:18:20 +0200

    new styles added

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -3,11 +3,11 @@ import "../styles/topBar.css";
 export default function TopBar() {
     return (
         <>
-            <div>
+            <div class="flex flex-grow basis-0">
 				<a href="/">Dansalma</a>
 			</div>
 			<nav>
-                <ul className="navBar" >
+                <ul className="topBar" >
                     <li><a href="/">Inicio</a></li>
                     <li><a href="/">Sesiones</a></li>
                     <li><a href="/">Sobre Mi</a></li>
@@ -15,9 +15,13 @@ export default function TopBar() {
                     <li><a href="/">Contacto</a></li>
                 </ul>
             </nav>
-			<div>
-				<button>Hola</button>
-			</div>
+            <div class="flex flex-grow justify-end basis-0">
+                <button class="cssbuttons-io-button"> Apuntate a una sesión
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="2rem" height="1.5rem"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+                    </div>
+                </button>
+            </div>
         </>
     );
 }
