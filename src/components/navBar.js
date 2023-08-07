18e153e53a6cfd4d2a0894a027f540d3419e7eb6
Marc <marc.perez.guerrero@estudiantat.upc.edu>
2023-08-07 18:07:14 +0200

    Full TopBar fixes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,15 +1,21 @@
 // JavaScript
 const button = document.getElementById("button");
 const menuDesplegable = document.querySelector('#hiddenMenu');
-const checkbox = document.getElementById("menuTrigger");
+const svgAnimation = document.getElementById("trigger");
 console.log(button);
-console.log(checkbox);
 
 // Añade el event listener solo una vez, no es necesario tenerlo en línea y con addEventListener.
 button.addEventListener("click", () => {
     event.preventDefault();
     console.log("Button was clicked!");
     menuDesplegable.classList.toggle("active");
+    svgAnimation.classList.toggle("animation-trigger");
+});
+
+menuDesplegable.addEventListener("click", () => {
+    event.preventDefault();
+    menuDesplegable.classList.toggle("active");
+    svgAnimation.classList.toggle("animation-trigger");
 });
 
 
